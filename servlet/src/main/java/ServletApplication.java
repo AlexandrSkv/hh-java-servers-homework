e5d738818bc8b585ab5263b6cf08787b35fd4aa5
sasha_rus <sasha_rus@list.ru>
2022-03-25 10:36:54 +0300

    Task1

--- servlet/src/main/java/ServletApplication.java
+++ servlet/src/main/java/ServletApplication.java
@@ -1,6 +1,22 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+
 public class ServletApplication {
 
-  public static void main(String[] args) {
+  private static Server createServer(int port) {
+    Server server = new Server(port);
+    ServletHandler servletHandler = new ServletHandler();
+    servletHandler.addServletWithMapping(ServletCounter.class, "/counter/*");
+    servletHandler.addFilterWithMapping(ServletCoockieFilter.class, "/counter/clear", 0);
+    server.setHandler(servletHandler);
+    return server;
+  }
+
+  public static void main(String[] args) throws Exception {
+    int port = 8081;
+    Server server = createServer(port);
+    server.start();
+    server.join();
     // run, Jetty, run!
   }
 }
---
+++ servlet/src/main/java/ServletCounter.java
@@ -0,0 +1,105 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ServletCounter extends HttpServlet {
+
+    private int counter = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setHeader("Content-Type", "text/html; charset=UTF-8");
+        PrintWriter writer = response.getWriter();
+        switch (request.getRequestURI()) {
+            case ("/counter"): {
+                response.setStatus(HttpServletResponse.SC_OK);
+                writer.print("<h1>Текущие состояние счетчика: " + counter + "</h1>");
+                writer.flush();
+                break;
+            }
+            case ("/counter/clear"):
+            {
+                response.setStatus(HttpServletResponse.SC_OK);
+                writer.print("<h1>Используйте POST для обнуления счетчика</h1>");
+                writer.flush();
+                break;
+            }
+            default: {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            }
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setHeader("Content-Type", "text/html; charset=UTF-8");
+        PrintWriter writer = response.getWriter();
+        switch (request.getRequestURI()) {
+            case ("/counter"):
+            {
+                counter += 1;
+
+                response.setStatus(HttpServletResponse.SC_OK);
+                writer.print("<h1>Значение счетчика увеличено</h1>");
+                writer.flush();
+                break;
+            }
+            case ("/counter/clear"):
+            {
+                counter = 0;
+
+                response.setStatus(HttpServletResponse.SC_OK);
+                writer.print("<h1>Значение счетчика обнулено</h1>");
+                writer.flush();
+                break;
+            }
+            default:
+            {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            }
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setHeader("Content-Type", "text/html; charset=UTF-8");
+        PrintWriter writer = response.getWriter();
+
+        switch (request.getRequestURI()) {
+            case ("/counter"):
+            {
+                String value = request.getHeader("Subtraction-Value");
+                if (value != null) {
+                    try {
+                        counter = counter - Integer.parseInt(value);
+                        response.setStatus(HttpServletResponse.SC_OK);
+                        writer.print("<h1>Значение счетчика уменьшено</h1>");
+                        writer.flush();
+                    } catch (NumberFormatException e){
+                        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                        writer.print("<h1>Значение заголовка Subtraction-Value должно быть числом</h1>");
+                        writer.flush();
+                    }
+                }
+                else {
+                    response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                    writer.print("<h1>Нет заголовка Subtraction-Value</h1>");
+                    writer.flush();
+                }
+                break;
+            }
+            case ("/counter/clear"):
+            {
+                response.setHeader("Allow" , "POST");
+                response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+                break;
+            }
+            default:
+            {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            }
+        }
+    }
+}
